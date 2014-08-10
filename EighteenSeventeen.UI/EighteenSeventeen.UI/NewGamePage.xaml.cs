510e84baf823fcb2a909af547d46d6d6123cb3db
Paul Batum <paul.batum@gmail.com>
2014-08-10 00:52:45 -0700

    trying out Xamarin.Forms XAML

---
+++ EighteenSeventeen.UI/EighteenSeventeen.UI/NewGamePage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using ReactiveUI;
+using System.Linq;
+
+namespace EighteenSeventeen.UI
+{	
+	public partial class NewGamePage : ContentPage, IViewFor<NewGameViewModel>
+	{	
+		public NewGameViewModel ViewModel { get; set; }
+
+		object IViewFor.ViewModel {
+			get { return ViewModel; }
+			set { ViewModel = (NewGameViewModel)value; }
+		}
+
+		public NewGamePage ()
+		{
+			InitializeComponent ();
+
+			this.ViewModel = new NewGameViewModel ();
+			//playerListView.ItemsSource = this.ViewModel.Players;
+
+			this.ViewModel.AddPlayer.Subscribe (_ => {
+				this.ViewModel.Players.Add(this.ViewModel.NewPlayerName);
+				this.ViewModel.NewPlayerName = string.Empty;
+			});
+
+			this.ViewModel.RandomizeOrder.Subscribe (_ => {
+				var r = new Random();
+				var newOrder = this.ViewModel.Players.OrderBy(x => r.NextDouble()).ToList();
+				this.ViewModel.Players.Clear();
+				this.ViewModel.Players.AddRange(newOrder);
+			});
+
+			this.Bind (this.ViewModel, vm => vm.NewPlayerName, view => view.playerNameEntry.Text);
+			this.BindCommand (this.ViewModel, vm => vm.AddPlayer, view => view.addPlayerButton);
+			this.BindCommand (this.ViewModel, vm => vm.AddPlayer, view => view.playerNameEntry, "Completed");
+			this.BindCommand (this.ViewModel, vm => vm.StartGame, view => view.startButton);
+			this.BindCommand (this.ViewModel, vm => vm.RandomizeOrder, view => view.randomizeButton);
+		}
+	}
+}
+
---
+++ EighteenSeventeen.UI/EighteenSeventeen.UI/ViewModels/NewGameViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ReactiveUI;
+using System.Reactive.Linq;
+
+namespace EighteenSeventeen.UI
+{
+	public class NewGameViewModel : ReactiveObject
+	{
+		public ReactiveList<string> Players { get; private set; }
+
+		public ReactiveCommand<Object> AddPlayer { get; private set; }
+		public ReactiveCommand<Object> StartGame { get; private set; }
+		public ReactiveCommand<Object> RandomizeOrder { get; private set; }
+
+		string newPlayerName;
+		public string NewPlayerName {
+			get { return newPlayerName; }
+			set { this.RaiseAndSetIfChanged(ref newPlayerName, value); }
+		}
+
+		public NewGameViewModel()
+		{
+			Players = new ReactiveList<string> ();
+
+			var playerCount = this.WhenAnyValue (x => x.Players.Count);
+
+			AddPlayer = ReactiveCommand.Create (this.WhenAnyValue(x => x.Players.Count, x => x.NewPlayerName, 
+				(count, newPlayerName) => count < 7 && !string.IsNullOrEmpty(newPlayerName)));
+			StartGame = ReactiveCommand.Create (playerCount.Select(count => count >= 3));
+			RandomizeOrder = ReactiveCommand.Create (playerCount.Select(count => count >= 2));
+		}
+	}
+}
+
