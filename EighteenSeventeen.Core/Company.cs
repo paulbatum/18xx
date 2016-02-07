18457ea8716eb944d39acffc326963b6ad6a5037
Paul Batum <paul.batum@gmail.com>
2016-02-07 01:47:01 -0800

    Start with basic types

---
+++ EighteenSeventeen.Core/Company.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EighteenSeventeen.Core
+{
+    public class Company
+    {
+        public CompanyIdentity Identity { get; }
+
+        public Company(CompanyIdentity id)
+        {
+            Identity = id;
+        }
+
+    }
+
+    public enum CompanyIdentity
+    {
+        Arcade,
+        Belt,
+        Shawmut
+    }
+
+    public class CompanyState
+    {
+        public int Money { get; }
+        public int Loans { get; }
+        public List<Train> Trains { get; }
+    }
+}
---
+++ EighteenSeventeen.Core/Train.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EighteenSeventeen.Core
+{
+    public class Train
+    {
+        public TrainType Type { get; }
+
+        public Train(TrainType type)
+        {
+            Type = type;
+        }
+    }
+
+    public enum TrainType
+    {
+        Two,
+        TwoPlus,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight
+    }
+}
